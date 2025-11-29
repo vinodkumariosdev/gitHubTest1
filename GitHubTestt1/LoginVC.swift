c78a4f16d48081faa759fa1541051827240ba551
Your Name <your@email.com>
2025-11-29 21:49:28 +0530

    loginAdded

--- GitHubTestt1/LoginVC.swift
+++ GitHubTestt1/LoginVC.swift
@@ -11,6 +11,7 @@ class LoginVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("login")
 
         // Do any additional setup after loading the view.
     }
